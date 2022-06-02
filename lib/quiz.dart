86cb78d45a9e33ac14baea40da71d268437a0d13
YashrajsinhYz <15110419022@atmiyauni.edu.in>
2022-06-02 23:00:00 +0530

    Listed_map

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -21,9 +21,11 @@ class Quiz extends StatelessWidget {
         Question(
           questions[questionIndex]['questionText'],
         ),
-        ...(questions[questionIndex]['option'] as List<String>).map(
+        ...(questions[questionIndex]['option'] as List<Map<String, Object>>)
+            .map(
           (answer) {
-            return Answer(answerQuestion, answer);
+            return Answer(
+                () => answerQuestion(answer['score']), answer['text']);
           },
         ).toList(),
       ],
--- lib/result.dart
+++ lib/result.dart
@@ -3,11 +3,30 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 8) {
+      resultText = 'You are awesome !!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty good !!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are...strange ?!';
+    } else {
+      resultText = 'So bad !!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        "Congratulations, You did it !",
+        resultPhrase,
         style: TextStyle(
           fontSize: 20.0,
         ),
